d048695e8f47e3772a3ebb401aaa3095d8c68f6b
Samuel Ebek <ebeksam@gmail.com>
2023-10-11 21:22:49 +0100

    fetch Api

--- nav.js
+++ nav.js
@@ -14,3 +14,31 @@ nav.setAttribute("data-visible", false)
 navToggle.setAttribute("aria-expanded", false)
 }
 })
+
+var year = new Date().getFullYear()
+document.getElementById("currentYear").innerHTML = year
+
+
+fetch("https://rickandmortyapi.com/api/character")
+.then(response => response.json())
+.then(data => makeCards(data.results))
+
+function makeCards(characterArray){
+    cardContainer = document.querySelector("#card-container")
+    console.log(characterArray)
+    characterArray.forEach(character => {
+        cardContainer.innerHTML = cardContainer.innerHTML + `
+        
+        <div class="card">
+        <img src=${character.image} alt="Arbolian">
+        <div class="desc">
+            <h2>${character.name}</h2>
+            <p>${character.status} - ${character.species}</p>
+            <p class="light-gray">Last known location:</p>
+            <p class="para">St. Gloopy Noops Hospital</p>
+            <p class="light-gray">First seen in:</p>
+            <p class="para get">Get Schwifty</p>
+        </div>
+        </div>`
+    })
+}
